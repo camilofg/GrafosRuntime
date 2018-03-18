e6c69a12c388fece4cb525dca4e1ca235de2d470
camilofg <rc.forero@uniandes.edu.co>
2018-03-18 15:48:20 -0500

    upload solution 2

---
+++ GrafosRuntime/Factory/IFactorySearch.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Factory
+{
+    public interface IFactorySearch
+    {
+        string CreateFactorySearch(XmlDocument configuratorFile);
+    }
+}
---
+++ GrafosRuntime/Factory/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Factory")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4c589edf-6755-4a61-8ae6-79e41732f463")]
